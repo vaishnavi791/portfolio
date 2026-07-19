94334bf9fadd1535b4e6e9fda01d6cb8a0a04d91
Vaishnavi Kumari <vaishnavigupta170@gmail.com>
2026-07-19 18:17:18 +0530

    style: fix theme alignments

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/portfolio/', 
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html'),
+      },
+    },
+  },
 });
